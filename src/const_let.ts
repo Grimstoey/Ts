2a435eb2b3e70988a268f2c71d6a38cf569a32d1
Wiwee <grimstoey@gmail.com>
2025-11-29 16:46:23 +0700

    Task_8

---
+++ src/const_let.ts
@@ -0,0 +1,74 @@
+//1.
+// let x = 10;
+// x = 15;
+// console.log(x); //15
+
+//2.
+// const x = 10;
+// x = 15; // Error
+//console.log(x);
+
+//3.
+// interface Person {
+//    name: string;
+//    age: number;
+// }
+
+// let a: Person = {
+//    name: 'Alice',
+//    age: 30
+// }
+
+// a = {
+//    name: 'Bob',
+//    age: 25
+// }
+// console.log(a); //{ name: 'Bob', age: 25 }
+
+
+//4.
+// interface Person {
+//    name: string;
+//    age: number;
+// }
+
+// const a: Person = {
+//    name: 'Alice',
+//    age: 30
+// }
+
+
+// a = {
+//    name: 'Bob',
+//    age: 25
+// }
+// console.log(a)
+//Cannot assign to 'a' because it is a constant.
+
+
+//5.
+// interface Person {
+//    name: string;
+//    age: number;
+// }
+
+
+// const a: Person = {
+//    name: 'Alice',
+//    age: 30
+// }
+
+
+// a.name = 'Bob'
+// a.age = 25
+// console.log(a) //{ name: 'Bob', age: 25 }
+
+//6.
+
+// const list = [1, 2, 3, 4, 5];
+// console.log(list); //[ 1, 2, 3, 4, 5 ]
+
+
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,57 @@
-console.log("Hello, TypeScript with Node.333!");
+
+// let x:number = 10
+// if (x > 10) {
+//   console.log('x is more than 10')
+// }
+
+
+// let x:number = 9
+// if (x > 10) {
+//   console.log('x is more than 10')
+// }else {
+//   console.log('x is less than 10')
+// }
+
+// let x = 35;
+// let y = 65;
+
+// if (x > 5 && y > 12) {
+//   console.log('ค่ามากจัง')
+// }
+// else {
+//   console.log((x*y) + (x/y))
+// }
+
+
+// let x =7
+// if (x > 10) {
+//   console.log('x is more than 10')
+// } else if (x < 5){
+//     console.log('x is less than 5')
+// }
+// else {
+//   console.log('x is between 5 and 10')
+// }
+
+
+let score = 60;
+
+if(score > 80)
+{
+    console.log('A');
+}
+else if(score > 60)
+{
+    console.log('B');
+}
+else if(score > 50)
+{
+    console.log('C');
+}
+else{
+    console.log('F');
+}
+
+
+
+
