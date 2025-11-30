0da436026ed8e94d46882e376a305ed49ab85759
Wiwee <grimstoey@gmail.com>
2025-11-30 09:54:45 +0700

    commit_Task10

---
+++ src/Task_10.ts
@@ -0,0 +1,24 @@
+// let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']  
+// for (const c of color) {  
+//   console.log(c);  
+// }
+
+// red
+// blue 
+// green
+// yellow
+// orange
+// purple
+
+let color = ['red', 'blue', 'green', 'yellow', 'orange', 'purple']  
+for (const index in color) {  
+  console.log(`Index: ${index}, Value: ${color[index]}`);  
+}
+
+// Index: 0, Value: red
+// Index: 1, Value: blue
+// Index: 2, Value: green
+// Index: 3, Value: yellow
+// Index: 4, Value: orange
+// Index: 5, Value: purple
+
