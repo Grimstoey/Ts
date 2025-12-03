c431311d80eb7242963df158f6b9bfea590d052d
Wiwee <grimstoey@gmail.com>
2025-12-03 14:25:48 +0700

    Commit_Task_12

---
+++ src/Task_12.ts
@@ -0,0 +1,32 @@
+// const greet = function greet(name : string){
+//     return 'Hello ' + name
+// }
+
+// console.log(greet('John'))
+// Hello John
+
+
+// const greet = (name : string) => 'Hello ' + name 
+
+// console.log(greet('John'));
+//Hello John
+
+// const multiply = (x : number,y : number) => { return x * y }
+
+// console.log(multiply(5, 10)); 
+//50
+
+// const compare = (x : number, y : number) => {
+//     if(x > y)
+//     {
+//         return x;
+//     }
+//     else
+//     {
+//         return y;
+//     }
+// };
+
+// console.log(compare(5, 7));
+// console.log(compare(20, 4));
+
